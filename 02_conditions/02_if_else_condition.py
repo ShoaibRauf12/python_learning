a716f15f316e9284eeb6daaf95719a1c936a022e
Shoaib Rauf <shoaibrauf125@gmail.com>
2026-08-18 19:44:06 +0500

    working on conditions

---
+++ 02_conditions/02_if_else_condition.py
@@ -0,0 +1,13 @@
+marks = 399
+
+
+if marks >= 600 :
+    print("Your position: 1st")
+elif marks > 550 :
+    print("Your position: 2nd")
+elif marks > 480 :
+    print("Your position: 3rd" )
+elif marks >= 400 :
+    print("Your position: Pass")
+else :
+    print("Your position: Fail")
