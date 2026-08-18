2c12cb2686375d8755eceac4885fee26042d1f14
Shoaib Rao <shoaib.rao@Liveadmins.com>
2026-08-18 19:16:45 +0500

    Working on Camparison_operators

---
+++ 01_basics/camparison_operators.py
@@ -0,0 +1,11 @@
+price1 = 55
+price2 = 20
+age1 = 18
+
+print("Price1 Grater than price2 ",price1 > price2) #True
+print("Price1 Less than price2 ",price1 < price2) #False
+
+print(age1 > 20 ) #False
+print(age1 < 20) #True
+
+print(age1 == 18) #True
