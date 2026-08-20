1fc1836b75316ea1fc8a22915087e7ef5d42211c
Shoaib Rauf <shoaibrauf125@gmail.com>
2026-08-20 21:48:17 +0500

    Added new files

---
+++ 03_loops/03_lists.py
@@ -0,0 +1,14 @@
+fruit_list = ["Apple","Mango","Banana","Orange","Watermallan","Peach","Strawberry"]
+
+print(fruit_list[0])
+print(fruit_list[1])
+print(fruit_list[2])
+print(fruit_list[3])
+print(fruit_list[4])
+print(fruit_list[5])
+print()
+
+language_list = ["Python","Java","C#","PHP","Node","Ruby","Next.js"]
+
+for lng in language_list :
+    print("Language Name is: ",lng)
---
+++ 03_loops/04_list_methods.py
@@ -0,0 +1,2 @@
+fruit_list = ["Apple","Mango","Banana"]
+
